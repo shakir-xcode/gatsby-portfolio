cff398b5bab4ec88f65834b68005c5827b4c2058
Shakir <shaker88999@gmail.com>
2023-04-28 08:45:33 +0530

    refactored code for gatsby-plugin-image

--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -1,35 +1,36 @@
 import React from 'react'
 import Layout from '../../components/Layout'
 import { Link, graphql } from 'gatsby';
-import GatsbyImage from 'gatsby-image';
+import { GatsbyImage } from "gatsby-plugin-image"
 import * as styles from '../../styles/projects.module.css';
 
 function index({ data }) {
 
-    const projects = data.projects.nodes;
-    const contact = data.contact.siteMetadata.contact;
+  const projects = data.projects.nodes;
+  console.log(projects)
+  const contact = data.contact.siteMetadata.contact;
+  // fluid={project.frontmatter.thumbs.childImageSharp.fluid}
+  return (
+    <Layout>
+      <div className={styles.projectContainer}>
+        <h1>My Projects</h1>
+        <div className={styles.projects} >
+          {projects.map(project => (
+            <Link to={'/projects/' + project.frontmatter.slug} key={project.key} >
+              <GatsbyImage image={project.frontmatter.thumbs.childImageSharp.gatsbyImageData} />
+              <h3>{project.frontmatter.title}</h3>
+              <p>{project.frontmatter.stack}</p>
+            </Link>
+          ))}
+        </div>
+        <div>
+          <p>Like what you see? contact me  {contact} for a quote!
+          </p>
+        </div>
+      </div>
+    </Layout>
 
-    return (
-        <Layout>
-            <div className={styles.projectContainer}>
-                <h1>My Projects</h1>
-                <div className={styles.projects} >
-                    {projects.map(project => (
-                        <Link to={'/projects/' + project.frontmatter.slug} key={project.key} >
-                            <GatsbyImage fluid={project.frontmatter.thumbs.childImageSharp.fluid} />
-                            <h3>{project.frontmatter.title}</h3>
-                            <p>{project.frontmatter.stack}</p>
-                        </Link>
-                    ))}
-                </div>
-                <div>
-                    <p>Like what you see? contact me  {contact} for a quote!
-                    </p>
-                </div>
-            </div>
-        </Layout>
-
-    )
+  )
 }
 
 export default index;
@@ -43,9 +44,7 @@ query projectsPage {
             slug
             thumbs {
               childImageSharp {
-                fluid {
-                    ...GatsbyImageSharpFluid
-                   }
+                gatsbyImageData(layout: FULL_WIDTH)
               }
             }
           }
